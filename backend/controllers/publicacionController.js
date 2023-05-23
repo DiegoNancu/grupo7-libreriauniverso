af8b97ba0d53400a4440d1af960eaae7393e0665
Manu <manuel.quijada1901@alumnos.ubiobio.cl>
2023-05-23 04:58:17 -0400

    fix controller

--- backend/controllers/publicacionController.js
+++ backend/controllers/publicacionController.js
@@ -4,6 +4,5 @@ const publi = require("../models/publicacion");
 const createPubli = (req, res) => {
     const { id_publicacion, id_producto, nombre_tienda, fecha, status, descripcion } = req.body;
     const newPublicacion = new publi({
-
     });
 }
