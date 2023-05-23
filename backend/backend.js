9db3964ebf0d9ec23d31f14a5ece459d04966f94
Dop1ta <gabrielrburgos@outlook.com>
2023-05-23 00:50:03 -0400

    add route backend

---
+++ backend/backend.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const proRoutes = require("./routes/producRoutes");
+
+mongoose.set('strictQuery', false);
+app.use(cors());
+app.use(express.json());
+app.options("*", cors());
+app.use("/api", proRoutes)
+
+
+
+const options = {
+  useNewUrlParser: true,
+  autoIndex: true,
+  keepAlive: true,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+  family: 4,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.DB, options, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port  ${process.env.PORT}`);
+});
