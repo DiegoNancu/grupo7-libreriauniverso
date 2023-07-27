1e3a1b261f53931bfefd011489e9d5fc698d88c2
Dop1ta <gabrielrburgos@outlook.com>
2023-07-27 01:10:15 -0400

    syncs

--- frontend/src/components/GirdPro.jsx
+++ frontend/src/components/GirdPro.jsx
@@ -1,106 +1,4 @@
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
-// import Products from './Product/Products';
-// import { styled } from '@mui/material/styles';
-
-// import FormControl from '@mui/material/FormControl';
-// import InputLabel from '@mui/material/InputLabel';
-// import Select from '@mui/material/Select';
-// import MenuItem from '@mui/material/MenuItem';
-// import Slider from '@mui/material/Slider';
-
-// function valuetext(value) {
-//   return `${value}°C`;
-// }
-
-// export default function FullWidthGrid() {
-
-//   const libraryCategories = ['Fantasía', 'Ciencia ficción', 'Romance', 'Misterio', 'Aventura'];
-
-//   const [selectedCategory, setSelectedCategory] = React.useState('');
-
-//   const handleCategoryChange = (event) => {
-//     setSelectedCategory(event.target.value);
-//   };
-
-//   const [value, setValue] = React.useState([20, 37]);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
-
-
-
-
-//   return (
-//     <Box>
-//       <Grid container spacing={1}>
-
-//         <Box
-//           sx={{
-//             backgroundColor: '#ffffff',
-//             marginTop: '8px',
-//             display: 'flex',
-//             justifyContent: 'center',
-//             borderRadius: '8px',
-//             boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-//             padding: '8px',
-//           }}
-//         >
-//           <Grid width={230}>
-//             <FormControl fullWidth>
-//               <InputLabel id="category-label">Categoría</InputLabel>
-//               <Select
-//                 labelId="category-label"
-//                 value={selectedCategory}
-//                 onChange={handleCategoryChange}
-//                 autoWidth
-//                 displayEmpty
-//                 sx={{
-//                   backgroundColor: '#ffffff',
-//                   '& .MuiSelect-icon': {
-//                     color: '#555555',
-//                   },
-//                 }}
-//               >
-//                 {libraryCategories.map((category, index) => (
-//                   <MenuItem key={index} value={category}>
-//                     {category}
-//                   </MenuItem>
-//                 ))}
-//               </Select>
-//             </FormControl>
-
-//             <Box sx={{ width: 190, marginTop: 5 }} >
-//               <InputLabel>Precio</InputLabel>
-//               <Slider
-//                 getAriaLabel={() => 'Temperature range'}
-//                 value={value}
-//                 onChange={handleChange}
-//                 valueLabelDisplay="auto"
-//                 getAriaValueText={valuetext}
-//               />
-//             </Box>
-//           </Grid>
-
-
-
-
-//         </Box>
-
-
-
-//         <Grid item marginLeft={2}>
-//           <Products />
-//         </Grid>
-//       </Grid>
-//     </Box>
-//   );
-// }
-
-
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Products from './Product/Products';
@@ -110,34 +8,59 @@ import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Slider from '@mui/material/Slider';
-import Stack from '@mui/material/Stack'; // Import the Stack component
+import Stack from '@mui/material/Stack';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button'; // Importa el componente Button de Material-UI
 
 function valuetext(value) {
   return `${value}°C`;
 }
 
 export default function FullWidthGrid() {
-  const libraryCategories = ['Electronica', 'Articulos de oficina', 'Juguetes', 'Servicios', 'Utiles escolares', 'Decoracion'];
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [priceRange, setPriceRange] = useState([20, 37]);
 
-  const [selectedCategory, setSelectedCategory] = React.useState('');
+  const getCategory = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/listCat');
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCategory();
+  }, []);
 
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
   };
 
-  const [value, setValue] = React.useState([20, 37]);
+  const handlePriceChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const applyFilters = () => {
+    // Aquí puedes utilizar "selectedCategory" y "priceRange" para filtrar los productos
+    // Llama a una función o realiza una petición para obtener los productos filtrados y actualizar la vista.
+    // Por ejemplo:
+    // Filtrar productos basados en la categoría seleccionada y el rango de precios
+    // Luego, actualiza el estado de los productos filtrados y muestra la vista actualizada
   };
 
   return (
     <Box>
       <Grid container spacing={1}>
-        <Stack // Use Stack to make the filter section responsive
-          direction={{ xs: 'column' }} // Vertical on xs (extra-small) screens, Horizontal on sm (small) and above screens
-          spacing={2} // Space between stacked elements
-          alignItems="center" // Align items in the center
+        <Stack
+          direction={{ xs: 'column' }}
+          spacing={2}
+          alignItems="center"
           sx={{
             backgroundColor: '#ffffff',
             marginTop: '8px',
@@ -161,24 +84,35 @@ export default function FullWidthGrid() {
                 },
               }}
             >
-              {libraryCategories.map((category, index) => (
-                <MenuItem key={index} value={category}>
-                  {category}
-                </MenuItem>
-              ))}
+              {loading ? (
+                <CircularProgress style={{ position: 'absolute', top: '50%', left: '50%' }} />
+              ) : (
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                  {data.categories &&
+                    data.categories.map((category, index) => (
+                      <MenuItem key={index} value={category.name}>
+                        {category.name}
+                      </MenuItem>
+                    ))}
+                </Grid>
+              )}
             </Select>
           </FormControl>
 
-          <Box sx={{ width: 190 }} >
+          <Box sx={{ width: 190 }}>
             <InputLabel>Precio</InputLabel>
             <Slider
               getAriaLabel={() => 'Temperature range'}
-              value={value}
-              onChange={handleChange}
+              value={priceRange}
+              onChange={handlePriceChange}
               valueLabelDisplay="auto"
               getAriaValueText={valuetext}
             />
           </Box>
+
+          <Button variant="contained" onClick={applyFilters}>
+            Aplicar filtros
+          </Button>
         </Stack>
 
         <Grid item marginLeft={2}>
