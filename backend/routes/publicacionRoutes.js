914e863bf23d4f44d1340f82873b99407a75c8bf
Manu <manuel.quijada1901@alumnos.ubiobio.cl>
2023-07-27 00:55:57 -0400

    fix

--- backend/routes/publicacionRoutes.js
+++ backend/routes/publicacionRoutes.js
@@ -6,5 +6,6 @@ api.post('/addPubli', publiController.createPublicacion);
 api.get('/listPubli', publiController.getPublicacion);
 api.put('/updatePubli/:id', publiController.updatePublicacion);
 api.delete('/deletePubli/:id', publiController.deletePublicacion);
+api.get('/getPubli/:id', publiController.getPublicacionById);
 
 module.exports = api;
