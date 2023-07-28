dcf03448056367b861fbd1e8428ea8e560877a4c
dop1ta <gabrielrburgos@outlook.com>
2023-07-27 22:27:11 -0400

    pc

--- frontend/src/components/GirdPro.jsx
+++ frontend/src/components/GirdPro.jsx
@@ -12,16 +12,31 @@ import axios from 'axios';
 import CircularProgress from '@mui/material/CircularProgress';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Drawer from '@mui/material/Drawer';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function valuetext(value) {
   return `${value}°C`;
 }
 
-export default function FullWidthGrid() {
+// Crear un tema personalizado
+const theme = createTheme();
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <FullWidthGrid />
+    </ThemeProvider>
+  );
+}
+
+function FullWidthGrid() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [priceRange, setPriceRange] = useState([0, 200000]); // Aumentar el rango del slider a [0, 500]
+  const [priceRange, setPriceRange] = useState([0, 200000]);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
 
   const getCategory = async () => {
     try {
@@ -39,7 +54,7 @@ export default function FullWidthGrid() {
   }, []);
 
   const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value); // Usar directamente el value del evento
+    setSelectedCategory(event.target.value);
   };
 
   const handlePriceChange = (event, newValue) => {
@@ -48,74 +63,159 @@ export default function FullWidthGrid() {
 
   const clearFilters = () => {
     setSelectedCategory('');
-    setPriceRange([0, 200000]); // Restablecer el rango del slider a [0, 500]
-    // Aquí puedes restablecer los demás estados relacionados con los filtros si los hay
+    setPriceRange([0, 200000]);
   };
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const isLargeScreen = useMediaQuery((theme) => theme.breakpoints.up('sm'));
 
   return (
     <Box>
       <Grid container spacing={1}>
-        <Stack
-          direction={{ xs: 'column' }}
-          spacing={2}
-          alignItems="center"
-          sx={{
-            backgroundColor: '#ffffff',
-            marginTop: '8px',
-            borderRadius: '8px',
-            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-            padding: '8px',
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel id="category-label">Categoría</InputLabel>
-            <Select
-              labelId="category-label"
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-              autoWidth
+        {!isLargeScreen && (
+          <Button onClick={toggleDrawer}>
+            {isDrawerOpen ? 'Cerrar filtros' : 'Abrir filtros'}
+          </Button>
+        )}
+        {isLargeScreen ? (
+          // Filtros mostrados normalmente en la página grande
+          <Box sx={{ width: 250, marginRight: '16px' }}>
+            <Stack
+              direction={{ xs: 'column' }}
+              spacing={2}
+              alignItems="center"
               sx={{
                 backgroundColor: '#ffffff',
-                '& .MuiSelect-icon': {
-                  color: '#555555',
-                },
+                marginTop: '8px',
+                padding: '8px',
               }}
             >
-              {loading ? (
-                <CircularProgress style={{ position: 'absolute', top: '50%', left: '50%' }} />
-              ) : (
-                <MenuItem value="">
-                  <em>Seleccione una categoría</em>
-                </MenuItem>
-              )}
-              {data.categories &&
-                data.categories.map((category) => (
-                  <MenuItem key={category._id} value={category._id}> {/* Usar directamente el _id como value */}
-                    {category.name}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
+              <FormControl fullWidth>
+                <InputLabel id="category-label">Categoría</InputLabel>
+                <Select
+                  labelId="category-label"
+                  value={selectedCategory}
+                  onChange={handleCategoryChange}
+                  autoWidth
+                  sx={{
+                    backgroundColor: '#ffffff',
+                    '& .MuiSelect-icon': {
+                      color: '#555555',
+                    },
+                  }}
+                >
+                  {loading ? (
+                    <CircularProgress style={{ position: 'absolute', top: '50%', left: '50%' }} />
+                  ) : (
+                    <MenuItem value="">
+                      <em>Seleccione una categoría</em>
+                    </MenuItem>
+                  )}
+                  {data.categories &&
+                    data.categories.map((category) => (
+                      <MenuItem key={category._id} value={category._id}>
+                        {category.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
 
-          <Box sx={{ width: 150 }}> {/* Reducir la anchura del slider */}
-            <InputLabel>Precio</InputLabel>
-            <Slider
-              getAriaLabel={() => 'Temperature range'}
-              value={priceRange}
-              onChange={handlePriceChange}
-              valueLabelDisplay="auto"
-              getAriaValueText={valuetext}
-              min={0} // Establecer el valor mínimo del slider a 0
-              max={200000} // Establecer el valor máximo del slider a 500
-            />
+              <Box sx={{ width: 150 }}>
+                <InputLabel>Precio</InputLabel>
+                <Slider
+                  getAriaLabel={() => 'Temperature range'}
+                  value={priceRange}
+                  onChange={handlePriceChange}
+                  valueLabelDisplay="auto"
+                  getAriaValueText={valuetext}
+                  min={0}
+                  max={200000}
+                />
+              </Box>
+
+              <Button
+                variant="contained"
+                onClick={clearFilters}
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Borrar filtros
+              </Button>
+            </Stack>
           </Box>
+        ) : (
+          // Filtros en el Drawer para pantallas pequeñas
+          <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer}>
+            <Stack
+              direction={{ xs: 'column' }}
+              spacing={2}
+              alignItems="center"
+              sx={{
+                backgroundColor: '#ffffff',
+                marginTop: '8px',
+                padding: '8px',
+                width: 250,
+              }}
+            >
+              <FormControl fullWidth>
+                <InputLabel id="category-label">Categoría</InputLabel>
+                <Select
+                  labelId="category-label"
+                  value={selectedCategory}
+                  onChange={handleCategoryChange}
+                  autoWidth
+                  sx={{
+                    backgroundColor: '#ffffff',
+                    '& .MuiSelect-icon': {
+                      color: '#555555',
+                    },
+                  }}
+                >
+                  {loading ? (
+                    <CircularProgress style={{ position: 'absolute', top: '50%', left: '50%' }} />
+                  ) : (
+                    <MenuItem value="">
+                      <em>Seleccione una categoría</em>
+                    </MenuItem>
+                  )}
+                  {data.categories &&
+                    data.categories.map((category) => (
+                      <MenuItem key={category._id} value={category._id}>
+                        {category.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
 
-          <Button variant="contained" onClick={clearFilters} color="error" startIcon={<DeleteIcon />}>
-            Borrar filtros
-          </Button>
-        </Stack>
+              <Box sx={{ width: 150 }}>
+                <InputLabel>Precio</InputLabel>
+                <Slider
+                  getAriaLabel={() => 'Temperature range'}
+                  value={priceRange}
+                  onChange={handlePriceChange}
+                  valueLabelDisplay="auto"
+                  getAriaValueText={valuetext}
+                  min={0}
+                  max={200000}
+                />
+              </Box>
 
-        <Grid item marginLeft={4}>
+              <Button
+                variant="contained"
+                onClick={clearFilters}
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Borrar filtros
+              </Button>
+            </Stack>
+          </Drawer>
+        )}
+
+        <Grid item marginLeft={isLargeScreen ? 4 : 0}>
           <Products selectedCategory={selectedCategory} priceRange={priceRange} />
         </Grid>
       </Grid>
