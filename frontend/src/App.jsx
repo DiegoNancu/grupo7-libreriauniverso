e2a3d7e8d9a18175e3192b1453972f501309d6db
DiegoNancu <d_nancupillan@hotmail.com>
2023-08-07 01:54:13 -0400

    avance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import ListProducts from "./pages/ListProducts";
 import AddProduct from "./pages/admin/adminProducts";
+import Chat from './pages/Chat';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ListProducts" element={<ListProducts />} />
         <Route path="/AddProduct" element={<AddProduct />} />
+        <Route path="/Chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
