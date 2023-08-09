0a74a8a6707daf7125f44cf34c3b9340c2c09732
Kortedie <diego.a.cortesgonzalez@gmail.com>
2023-08-09 05:55:50 -0400

    commit final

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -42,4 +42,45 @@ const sendEmail = async (req, res) => {
   }
 };
 
-module.exports = { sendEmail };
+const sendEmailSingUp = async (req, res) => {
+  const { name, email, password} = req.body;
+  const to = email; // Usar el email como destinatario
+  const subject = '¡Gracias por Registrarte en nuestra página web!'; // Asunto del correo
+
+  try {
+
+    const text = `Hola ${name},\n\nGracias por registrarte en nuestra página web. compra. No olvides que tu contraseña es ${password}. Si no te has registrado tú, ingresa a la página y contacta con la tienda.\n\n¡Te esperamos!\n\nSaludos,\nLibrería Universo`;
+
+    const email1 = new Email({ to, subject, text });
+    await email1.save();
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'libreriauniverso.tiendaonline@gmail.com',
+        pass: 'ifgopbnkcklkxxvj',
+      },
+    });
+
+    const mailOptions = {
+      from: 'libreriauniverso.tiendaonline@gmail.com', // Cambiar a tu correo
+      to,
+      subject,
+      text,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error al enviar el correo');
+      } else {
+        res.status(200).send('Correo enviado');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error interno del servidor');
+  }
+};
+
+module.exports = { sendEmail, sendEmailSingUp };
